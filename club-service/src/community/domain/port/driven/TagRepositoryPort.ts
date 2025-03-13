40c2355bea1b2af28824df57c342561ee821f3f5
nooojuan05 <jsanabriac54@gmail.com>
2025-03-12 20:43:23 -0500

    Arreglos solicitados realizados

--- club-service/src/community/domain/port/driven/TagRepositoryPort.ts
+++ club-service/src/community/domain/port/driven/TagRepositoryPort.ts
@@ -1,6 +1,7 @@
 import Repository2 from "../../repository/RepositoryInterface2";
+import CategoryDataInterface from "../../types/CategoryDataInterface";
 import TagDataInterface from "../../types/TagDataInterface";
 
 export default interface ITagRepository extends Repository2<string, TagDataInterface> {
-    
+    getCategoryById(id: string): Promise<CategoryDataInterface>;
 }
--- club-service/src/community/infrastructure/express/controller/CommunityController.ts
+++ club-service/src/community/infrastructure/express/controller/CommunityController.ts
@@ -19,10 +19,11 @@ export default class CommunityController implements CommunityControllerExpressPo
                 description: community.description,
                 tags: community.tags,
                 privacy: community.privacy,
-                category: community.category.getName(),
+                category: community.category,
                 rules: community.rules,
                 owner: community.creator,
                 members_number: community.number_members,
+                creation_date: community.creation_date,
                 image: community.image
             }
         })
@@ -30,10 +31,34 @@ export default class CommunityController implements CommunityControllerExpressPo
     }
 
     public async save(_req: Request, res: Response): Promise<void> {
-        const { name, descripcion, image, privacy, creation_date, creator_user, rules, category, tags } = _req.body;
-        await this.communityUseCaseSave.save(name, descripcion, image, privacy, creation_date, creator_user, rules,category, tags);
-        res.status(200).json({ message: 'Community created successfully' });
+        try {
+            const { name, descripcion, image, privacy, creation_date, creator_user, rules, category, tags } = _req.body;
+    
+            // Validar que todos los campos estén presentes
+            if (!name || !descripcion || !image || !privacy || !creator_user || !rules || !category || !tags) {
+                res.status(400).json({ error: "All fields are required: name, descripcion, image, privacy, creation_date, creator_user, rules, category, tags" });
+                return;
+            }
+            let cre
+            if (creation_date === undefined) {
+                const today = new Date();
+                cre = today.toISOString().split("T")[0];
+                
+            }else {
+                cre = creation_date
+            }
+            const rulesString = Array.isArray(rules) ? rules.join(", ") : rules;
+    
+            // Guardar la comunidad si pasa la validación
+            await this.communityUseCaseSave.save(name, descripcion, image, privacy, cre, creator_user, rulesString, category, tags);
+            res.status(200).json({ message: "Community created successfully" });
+    
+        } catch (error) {
+            console.error("Error creating community:", error);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
     }
+    
 
     public async getCommunityById(req: Request, res: Response): Promise<void> {
         let id = req.params['id']
@@ -48,10 +73,11 @@ export default class CommunityController implements CommunityControllerExpressPo
             description: community.description,
             tags: community.tags,
             privacy: community.privacy,
-            category: community.category.getName(),
+            category: community.category,
             rules: community.rules,
             owner: community.creator,
             members_number: community.number_members,
+            creation_date: community.creation_date,
             image: community.image
         }
 
